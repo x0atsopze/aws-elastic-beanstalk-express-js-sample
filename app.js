fc95d71245c00b05fc2da4a8927556ab7dd52f8c
Anderson T <andersontsopze+code@gmail.com>
2024-07-09 15:14:46 -0400

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello world from the DevOps Board'));
+app.get('/', (req, res) => res.send('DevOps To The Moon'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
